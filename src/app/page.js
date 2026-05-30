6068b0eeb1bd4fae3be11db3f73b748bea1dbf07
unknown <sholaaz1805@gmail.com>
2026-05-30 22:31:23 +0100

    home page updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,16 @@ import Hero from "@/components/home/Hero";
 import HomeFaq from "@/components/home/HomeFaq";
 import EngageLexora from "@/components/home/EngageLexora";
 import HeroStats from "@/components/home/Hero/HeroStats";
+import WhyChooseLexora from "@/components/home/Hero/WhyChooseLexora";
+import OurApproach from "@/components/home/Hero/OurApproach";
 
 export default function HomePage() {
   return (
     <main className="bg-ink text-paper overflow-hidden">
       <Hero />
       <HeroStats/>
+      <WhyChooseLexora/>
+      <OurApproach/>
       <HomeFaq />
       <EngageLexora />
     </main>
